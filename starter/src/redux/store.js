dec0d568c97ccd1cbe0f2f9441fa10c4afb0e290
Ken Okerlund <ken.okerlund@gmail.com>
2023-07-18 18:16:41 -0600

    Section 1 complete

---
+++ starter/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import potentialCountriesReducer from '../redux/slices/potentialCountriesSlice';
+
+
+
+export default configureStore({
+    reducer: {
+        potentialCountries: potentialCountriesReducer
+    },
+})
